e1d5471a2e797931f96986d4d1b088105b174248
anilyavas <anil.yavas99@gmail.com>
2024-11-07 18:13:14 +0300

    homescreen meditation list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
-import { FlatList, Text, View } from 'react-native';
-import { meditations } from '../data';
-import MeditationListItem from '../components/MeditationListItem';
+import { FlatList } from 'react-native';
+import { meditations } from '@/data';
+import MeditationListItem from '@/components/MeditationListItem';
 
 export default function App() {
   return (
     <FlatList
       data={meditations}
+      contentContainerClassName='gap-5 p-3'
+      className='bg-white'
       renderItem={({ item }) => <MeditationListItem item={item} />}
     />
   );
--- src/components/MeditationListItem.tsx
+++ src/components/MeditationListItem.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
-import { Meditation } from '../types';
+import { Meditation } from '@/types';
 
 export default function MeditationListItem({ item }: { item: Meditation }) {
   return (
-    <View className='p-5 m-5 border rounded-xl'>
+    <View className='p-5 border rounded-xl'>
       <Text className='font-semibold text-2xl'>{item.title}</Text>
     </View>
   );

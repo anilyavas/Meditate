d2ef5d6240eccc5f5e7ac390492e25136e7d7f2b
anilyavas <anil.yavas99@gmail.com>
2024-11-07 16:58:24 +0300

    types and dummy data

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,9 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View>
-      <Text>Hello</Text>
+      <Text className='font-bold text-4xl p-10 bg-red-500 color-blue-500'>
+        Home Screen
+      </Text>
       <StatusBar style='auto' />
     </View>
   );

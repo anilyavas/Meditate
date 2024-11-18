6f9d0a7f384bbfb3c4cdb6a012dc9ed01a007a75
anilyavas <anil.yavas99@gmail.com>
2024-11-18 18:56:45 +0300

    reanimated setup and install with some special animations for background

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -1,5 +1,5 @@
 import { router, useLocalSearchParams } from 'expo-router';
-import { Text, View, Pressable } from 'react-native';
+import { Text, View, Pressable, useWindowDimensions } from 'react-native';
 import { meditations } from '@/data';
 import {
   AntDesign,
@@ -11,13 +11,44 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Slider from '@react-native-community/slider';
 import { useAudioPlayer, useAudioPlayerStatus } from 'expo-audio';
 
+//@ts-ignore
 import audio from '../../../assets/meditations/audio1.mp3';
+import {
+  Easing,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
+import { useEffect } from 'react';
 
 export default function MeditationDetails() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const player = useAudioPlayer(audio);
   const status = useAudioPlayerStatus(player);
   const meditation = meditations.find((m) => m.id === Number(id));
+  const { height } = useWindowDimensions();
+
+  const top1 = useSharedValue(height * 0.2);
+  const top2 = useSharedValue(height * 0.45);
+  const top3 = useSharedValue(height * 0.7);
+
+  useEffect(() => {
+    const options = {
+      duration: 4000,
+      easing: Easing.bezier(0.5, 0, 0.5, 1),
+    };
+
+    top1.value = withRepeat(withTiming(height * 0.15, options), -1, true);
+    top2.value = withDelay(
+      1000,
+      withRepeat(withTiming(height * 0.4, options), -1, true)
+    );
+    top3.value = withDelay(
+      2000,
+      withRepeat(withTiming(height * 0.65, options), -1, true)
+    );
+  }, []);
 
   const formatSeconds = (milliseconds: number) => {
     const minutes = Math.floor(milliseconds / 60000);
@@ -32,6 +63,11 @@ export default function MeditationDetails() {
   return (
     <SafeAreaView className='bg-orange-400 flex-1 p-2 justify-between'>
       <View className='flex-1'>
+        <View className='absolute top-0 left-0 right-0 bottom-0 items-center'>
+          <View className='w-[400%] absolute aspect-square bg-yellow-400 rounded-full' />
+          <View className='w-[400%] absolute aspect-square top-[50%] bg-yellow-300 rounded-full' />
+          <View className='w-[400%] absolute aspect-square top-[70%] bg-orange-500 rounded-full' />
+        </View>
         {/* Header */}
         <View className='flex-1'>
           <View className='flex-row justify-between p-10 items-center'>
